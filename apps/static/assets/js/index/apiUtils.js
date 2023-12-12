1a00deb557abf890af1c495aec7e5b1fe5ffffcd
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-12-12 05:03:31 +0000

    Agrego mas emtodos para apis

--- apps/static/assets/js/index/apiUtils.js
+++ apps/static/assets/js/index/apiUtils.js
@@ -1,34 +1,16 @@
 // apiUtils.js
 const apiUrl = 'http://62.72.11.15:3000/api/';
-function buildUrl(endpoint) {
-    if(endpoint.charAt(endpoint.length - 1) !== "/"){
-        endpoint += "/"
+function buildUrl(endpoint, id = null) {
+    let url = apiUrl + endpoint;
+    if (id !== null) {
+        url += `/${id}`;
     }
-    return apiUrl + endpoint;
+    if (url.charAt(url.length - 1) !== "/") {
+        url += "/";
+    }
+    return url;
 }
 // Funcion para GET con fetch de algun endpoint
-/*
-async function fetchData(endpoint) {
-    try {
-        const response = await fetch(buildUrl(endpoint));
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const contentType = response.headers.get("content-type");
-        if (!contentType || !contentType.includes("application/json")) {
-            throw new Error("Response is not in JSON format");
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error.message);
-        throw error;
-    }
-}*/
-
 async function fetchData(endpoint) {
     try {
         const response = await fetch(buildUrl(endpoint));
@@ -51,30 +33,7 @@ async function fetchData(endpoint) {
 }
 
 // Funcion para POST con fetch de algun endpoint y un json body
-/*
-async function fetchSendPostData(endpoint, data) {
-    const url = `http://62.72.11.15:3000/api/${endpoint}`;
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    };
-
-    return fetch(url, options)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error al enviar datos a ${endpoint}`);
-            }
-            return response;
-        })
-        .catch(error => {
-            console.error(error.message);
-        });
-}*/
-
-async function fetchSendPostData(endpoint, data) {
+async function PostData(endpoint, data) {
     const url = buildUrl(endpoint);
     const options = {
         method: 'POST',
@@ -96,6 +55,63 @@ async function fetchSendPostData(endpoint, data) {
         });
 }
 
+// Función para PUT con fetch de algún endpoint y un json body
+async function putData(endpoint, id, data) {
+    const url = buildUrl(endpoint, id);
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    return fetch(url, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error al actualizar datos en ${endpoint}`);
+            }
+            return response;
+        })
+        .catch(error => {
+            console.error(error.message);
+        });
+}
+
+// Función para DELETE con fetch de algún endpoint
+async function deleteData(endpoint, id) {
+    const url = buildUrl(endpoint, id);
+    const options = {
+        method: 'DELETE',
+    };
+
+    return fetch(url, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error al eliminar datos en ${endpoint}`);
+            }
+            return response;
+        })
+        .catch(error => {
+            console.error(error.message);
+        });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Utils para despues
 // Funcion para calcular cuatos usuarios hay(tamaño)
 async function obtenerTotalUsuarios() {
     try {
